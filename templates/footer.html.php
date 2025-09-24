f170753b5e0a89a638defb76eb6ff29167e1a197
Sascha Schneider <sir.foomy@googlemail.com>
2025-09-24 10:59:50 +0200

    creates general layout & design

---
+++ templates/footer.html.php
@@ -0,0 +1,15 @@
+    </div>
+
+    <footer class="container">
+        <div id="footer-content" class="row">
+            <blockquote class="col-5">
+                <div class="quote">
+                    The best programs are the ones written when the programmer<br>
+                    is supposed to be working on something else.
+                </div>
+                <div class="author">Melinda Varian</div>
+            </blockquote>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ templates/header.html.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title><?= $title ?></title>
+
+    <link rel="stylesheet" href="css/app.css">
+    <script src="js/app.js"></script>
+    <script src="js/bootstrap/bootstrap.js"></script>
+</head>
+<body>
+    <header class="container">
+        <div id="header-content" class="row m-2">
+            <div class="col-2">
+                <img src="https://placecats.com/150/150" alt="fancy logo" class="logo">
+            </div>
+            <div class="titles col-10">
+                <h1><?= $title ?></h1>
+                <?php if (isset($subTitle)): ?>
+                <h2><?= $subTitle ?></h2>
+                <?php endif; ?>
+            </div>
+        </div>
+    </header>
+
+    <nav id="main"></nav>
+
+    <div id="content" class="container p-2">
